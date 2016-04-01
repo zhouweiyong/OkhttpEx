149f757053a27dd0cd0ccfe266207470fb69a260
zhouweiyong <16681805@qq.com>
2016-04-01 14:46:31 +0800

    调情取消请求

--- okhttp/src/main/java/com/huika/xokhttp/OkhttpError.java
+++ okhttp/src/main/java/com/huika/xokhttp/OkhttpError.java
@@ -19,6 +19,7 @@ public class OkhttpError {
      * 300：解析错误
      * 600:自定义错误，或其它未知错误，需要进一步明确
      * 700:链接超时
+     * 705:链接关闭
      */
     public int errorCode;//错误码
 
@@ -45,7 +46,10 @@ public class OkhttpError {
                 errorCode = 300;
             } else if (exception.getMessage().contains("timeout")) {
                 errorCode = 700;
-            } else {
+            }else if(exception.getMessage().contains("Socket closed")|| exception.getMessage().contains("socket closed")){//java.net.SocketException: Socket closed
+                errorCode = 705;
+            }
+            else {
                 errorCode = 600;
             }
         } else {
